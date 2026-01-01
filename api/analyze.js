8718c232cd51383bea33433179d8c06be6e4ca42
thefloorpro-spec <thefloorpro@gmail.com>
2026-01-01 15:52:56 -0800

    Update analyze.js

--- api/analyze.js
+++ api/analyze.js
@@ -1,4 +1,4 @@
-module.exports = async function handler(req, res) {
+export default async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
@@ -60,4 +60,4 @@ module.exports = async function handler(req, res) {
     console.error('Server error:', error);
     return res.status(500).json({ error: 'Internal server error' });
   }
-};
+}
